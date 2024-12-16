2c7d2e053e1f43f295233c6b9b09c768fa790bb4
Yoana Naydenova <y.naydenova@gmail.com>
2024-12-16 11:07:03 +0300

    Fix ExchangeRateClientFailureException

--- src/main/java/com/yoanan/foreignexchangeapp/exceptions/ExchangeRateClientFailureException.java
+++ src/main/java/com/yoanan/foreignexchangeapp/exceptions/ExchangeRateClientFailureException.java
@@ -1,4 +1,10 @@
 package com.yoanan.foreignexchangeapp.exceptions;
 
 public class ExchangeRateClientFailureException extends RuntimeException {
+    public ExchangeRateClientFailureException(String message) {
+        super(message);
+    }
+    public ExchangeRateClientFailureException(){
+        this("Exchange rate service failed");
+    }
 }
