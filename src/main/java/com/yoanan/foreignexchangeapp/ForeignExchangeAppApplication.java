8f11b3fa5fc72fbb3c9d46740319a4265a4db883
Yoana Naydenova <y.naydenova@gmail.com>
2024-12-22 17:38:00 +0300

    Fix ForeignExchangeAppApplication

--- src/main/java/com/yoanan/foreignexchangeapp/ForeignExchangeAppApplication.java
+++ src/main/java/com/yoanan/foreignexchangeapp/ForeignExchangeAppApplication.java
@@ -1,9 +1,12 @@
 package com.yoanan.foreignexchangeapp;
 
+import com.yoanan.foreignexchangeapp.config.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ConfigProperties.class)
 public class ForeignExchangeAppApplication {
 
     public static void main(String[] args) {
