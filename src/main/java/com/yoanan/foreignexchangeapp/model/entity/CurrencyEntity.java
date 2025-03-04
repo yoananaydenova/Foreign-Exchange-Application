cbc90f9f47f35c79ba196784d0281241a1dcbda8
Yoana Naydenova <y.naydenova@gmail.com>
2025-03-04 15:21:25 +0300

    Add CurrencyEntity

---
+++ src/main/java/com/yoanan/foreignexchangeapp/model/entity/CurrencyEntity.java
@@ -0,0 +1,43 @@
+package com.yoanan.foreignexchangeapp.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="codes")
+public class CurrencyEntity {
+
+    private String code;
+    private String name;
+
+
+    public CurrencyEntity() {
+    }
+
+    public CurrencyEntity(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    @Id
+    @Column(name="code", nullable = false, updatable = false)
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Column(name="name", nullable = false, updatable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
