9638d1bbfd53f11debb4ccc19221cb677a475a01
Yoana Naydenova <y.naydenova@gmail.com>
2025-03-16 15:06:48 +0300

    Fix ProviderCodeBindingModel

--- src/main/java/com/yoanan/foreignexchangeapp/model/binding/ProviderCodeBindingModel.java
+++ src/main/java/com/yoanan/foreignexchangeapp/model/binding/ProviderCodeBindingModel.java
@@ -1,4 +1,32 @@
 package com.yoanan.foreignexchangeapp.model.binding;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class ProviderCodeBindingModel {
+
+    private String result;
+    @JsonProperty("supported_codes")
+    private List<List<String>> supportedCodes;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public List<List<String>> getSupportedCodes() {
+        return supportedCodes;
+    }
+
+    public void setSupportedCodes(List<List<String>> supportedCodes) {
+        this.supportedCodes = supportedCodes;
+    }
+
+    public boolean isSuccess() {
+        return "success".equals(result);
+    }
 }
