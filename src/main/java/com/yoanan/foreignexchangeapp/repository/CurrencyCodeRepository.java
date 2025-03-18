880dbe049e66f9df77a761e56281e7d216d27586
Yoana Naydenova <y.naydenova@gmail.com>
2025-03-18 14:08:12 +0300

    Rename CurrencyRepository to CurrencyCodeRepository

--- src/main/java/com/yoanan/foreignexchangeapp/repository/CurrencyCodeRepository.java
+++ src/main/java/com/yoanan/foreignexchangeapp/repository/CurrencyCodeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CurrencyRepository extends JpaRepository<CurrencyCodeEntity, String> {
+public interface CurrencyCodeRepository extends JpaRepository<CurrencyCodeEntity, String> {
 }
