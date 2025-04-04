6a147e6d4d341daa10c327be94cd7589ef5808a0
Yoana Naydenova <y.naydenova@gmail.com>
2025-04-04 09:57:36 +0300

    Add scheduledCurrencyCodesImport method, add logService usage

--- src/main/java/com/yoanan/foreignexchangeapp/service/impl/CurrencyCodeServiceImpl.java
+++ src/main/java/com/yoanan/foreignexchangeapp/service/impl/CurrencyCodeServiceImpl.java
@@ -4,6 +4,7 @@ import com.yoanan.foreignexchangeapp.model.binding.ProviderCodeBindingModel;
 import com.yoanan.foreignexchangeapp.model.entity.CurrencyCodeEntity;
 import com.yoanan.foreignexchangeapp.repository.CurrencyCodeRepository;
 import com.yoanan.foreignexchangeapp.service.CurrencyCodeService;
+import com.yoanan.foreignexchangeapp.service.LogService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,33 +12,63 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CurrencyCodeServiceImpl implements CurrencyCodeService {
-    private static final Logger log = LoggerFactory.getLogger(CurrencyCodeServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyCodeServiceImpl.class);
 
     private final CurrencyCodeRepository currencyCodeRepository;
     private final WebClient webClient;
+    private final LogService logService;
 
     @Value("${provider.access-key}")
     private String apiKey;
 
 
-    public CurrencyCodeServiceImpl(CurrencyCodeRepository codeRepository, WebClient webClient) {
+    public CurrencyCodeServiceImpl(CurrencyCodeRepository codeRepository, WebClient webClient, LogService logService) {
         this.currencyCodeRepository = codeRepository;
         this.webClient = webClient;
+        this.logService = logService;
+    }
+
+    @Scheduled(cron = "${currency-codes.sync-cron}") // 1-st day of the month at 00:00
+    public void scheduledCurrencyCodesImport() {
+        logService.createLog(
+                "Scheduled Job",
+                "scheduledCurrencyCodesImport",
+                this.getClass().getSimpleName(),
+                "Starting currency code sync job"
+        );
+        try {
+            importCurrencyCodes();
+        } finally {
+            logService.createLog(
+                    "Scheduled Job",
+                    "scheduledImport",
+                    this.getClass().getSimpleName(),
+                    "Completed currency code sync job"
+            );
+        }
     }
 
     @Override
-    @Scheduled(cron = "${currency-codes.sync-cron}") // 1-st day of the month at 00:00
     @PostConstruct
     public void importCurrencyCodes() {
+        String methodName = new Object() {}
+                .getClass()
+                .getEnclosingMethod()
+                .getName();
+
+        String className = this.getClass().getSimpleName();
+        String requestUrl = "https://v6.exchangerate-api.com/v6/{apiKey}/codes";
+
         try {
             ProviderCodeBindingModel response = webClient.get()
-                    .uri("https://v6.exchangerate-api.com/v6/{apiKey}/codes", apiKey)
+                    .uri(requestUrl, apiKey)
                     .retrieve()
                     .bodyToMono(ProviderCodeBindingModel.class)
                     .block(Duration.ofSeconds(30));
@@ -53,10 +84,30 @@ public class CurrencyCodeServiceImpl implements CurrencyCodeService {
                 currencyCodeRepository.deleteAll();
                 currencyCodeRepository.saveAll(currencies);
 
-                log.info("Successfully synced {} currencies", currencies.size());
+                String successMessage = """
+                        Successfully synced %d currencies""".formatted(currencies.size());
+
+                LOGGER.info(successMessage);
+                logService.createLog(
+                        requestUrl,
+                        methodName,
+                        className,
+                        successMessage
+                );
             }
         } catch (Exception e) {
-            log.error("Failed to sync currencies", e);
+
+            String errorMessage = """
+                    Failed to sync currencies: %s""".formatted(e.getMessage());
+
+            LOGGER.error(errorMessage, e);
+
+            logService.createLog(
+                    requestUrl,
+                    methodName,
+                    className,
+                    errorMessage
+            );
         }
     }
 }
